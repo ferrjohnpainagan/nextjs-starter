1124db63d5b5f85a9eb4d8548497bae68e91fd5d
MamadK9 <mohammad.kp8093@gmail.com>
2023-05-06 11:24:03 +0330

    redux store modified

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,14 @@
-import { wrapper } from '@redux/store';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { store } from '@redux/store';
 import { appWithTranslation } from 'next-i18next';
 import { ThemeProvider } from 'next-themes';
 import '../styles/globals.css';
 
-export const App = ({ Component, ...rest }: AppProps) => {
-  const { store, props } = wrapper.useWrappedStore(rest);
+export const App = ({ Component, ...props }: AppProps) => {
   const { pageProps } = props;
   let persistor = persistStore(store);
 
@@ -28,4 +27,4 @@ export const App = ({ Component, ...rest }: AppProps) => {
   );
 };
 
-export default appWithTranslation(wrapper.withRedux(App));
+export default appWithTranslation(App);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,5 @@
 import { apiMiddleware } from '@api/apiMiddleware';
 import { configureStore } from '@reduxjs/toolkit';
-import { setupListeners } from '@reduxjs/toolkit/query';
-import { createWrapper } from 'next-redux-wrapper';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './rootReducer';
@@ -14,20 +12,13 @@ const persistConfig = {
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const makeStore = () =>
-  configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: false, //TODO CHECK THIS
-      }).concat(apiMiddleware),
-  });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, //TODO CHECK THIS
+    }).concat(apiMiddleware),
+});
 
-type Store = ReturnType<typeof makeStore>;
-setupListeners(makeStore().dispatch);
-
-export type RootState = ReturnType<Store['getState']>;
-export type AppDispatch = Store['dispatch'];
-
-export type AppStore = ReturnType<typeof makeStore>;
-export const wrapper = createWrapper<AppStore>(makeStore, { debug: false });
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
